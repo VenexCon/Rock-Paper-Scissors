2a80051db1ee94671399219407d2a02dc2f90765
Connor Davies <venexconsultant@outlook.com>
2022-03-22 22:27:33 +0000

    <feat> completed gamePlay

--- scripts/rpsjS.js
+++ scripts/rpsjS.js
@@ -2,10 +2,59 @@
 // create a function that returns at random, rock paper or scissors. 
 // computerPlayer should be first function 
 
-function computerPlay() {
-        let rPS = ["rock", "paper", "scissors"];
-        const compChoice = Math.floor(Math.random() * rPS.length);
-        return compChoice
-}
+let playerScore = 0;
+let computerScore = 0; 
 
-console.log (computerPlay())
+        function computerPlayer() {
+        let rPS = ["rock", "paper", "scissors"];
+        let compChoice = Math.floor(Math.random() * rPS.length);
+        
+        return compChoice == 0 ? "rock" :
+                compChoice == 1 ? "Scissors" :
+                "Paper";   
+        }
+        
+// computerSelection = computerPlay(); 
+        computerSelection = computerPlayer(); 
+        playerSelection = "scissors"; 
+
+// playRound decides the winner
+        function playRound(playerSelection, computerSelection) {
+                if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+                        return "DRAW!, Please try again"; 
+                } else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "SCISSORS"){
+                        console.log("Nice! You have won this round!")
+                         return ++playerScore;
+                } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
+                        console.log("Excellent, good job, you have won!")
+                         return ++playerScore;
+                } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
+                        console.log("Great job! you rock!")
+                         return ++playerScore;
+                }  else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "PAPER"){
+                        console.log("Unlucky you lost!")
+                         return ++computerScore;
+                } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
+                        console.log("Too bad, better luck next time")
+                         return ++computerScore;
+                } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") 
+                        console.log("Better luck next time buddy!")
+                         return ++computerScore;
+
+        }
+
+
+
+// results logged in console
+        console.log(playerSelection);
+        console.log(computerSelection)
+    
+        console.log(playerScore)
+        console.log(computerScore)
+         
+
+// gameRound() Function Dec 
+
+        function gameRound(playerChoice) {
+
+        }
