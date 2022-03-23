4d7d9f8656bf68c12d4fe07846548e938bd9b0c4
Connor Davies <venexconsultant@outlook.com>
2022-03-23 21:48:55 +0000

    <fix> Assigned result to a variable

--- scripts/rpsjS.js
+++ scripts/rpsjS.js
@@ -13,62 +13,68 @@
                 "PAPER";   
         }
         
-// computerSelection = computerPlay(); 
+    // computerSelection = computerPlay(); 
         computerSelection = computerPlayer(); 
         playerSelection = prompt(); 
 
-// playRound decides the winner
+    // playRound decides the winner
         function playRound(playerSelection, computerSelection) {
                 if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-                        console.log("DRAW!, Please try again");
+                        console.log("DRAW");
                         return null; 
                 } else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "SCISSORS"){
-                        console.log("Nice! You have won this round!")
+                        console.log("Won")
                         return true;
                 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
-                        console.log("Excellent, good job, you have won!")
+                        console.log("Won")
                         return true;
                 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
-                        console.log("Great job! you rock!")
+                        console.log("Won")
                         return true;
                 }  else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "PAPER"){
-                        console.log("Unlucky you lost!")
+                        console.log("Loss")
                         return false;
                 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
-                        console.log("Too bad, better luck next time")
+                        console.log("Loss")
                         return false;;
                 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") 
-                        console.log("Better luck next time buddy!")
+                        console.log("loss")
                         return false;
 
-}
+        }
                
 
 
 
-// results logged in console
+    // results logged in console
        // console.log(playerSelection);
         //console.log(computerSelection)
         //console.log(playerScore)
         //console.log(computerScore)
          
 
-// gameRound() Function Dec 
-let playerScore = 0;
-let computerScore = 0; 
-let drawCounter = 0; 
+    // gameRound() Function Dec 
+    let playerScore = 0;
+    let computerScore = 0; 
+    let drawCounter = 0; 
 
 
         function gameRound() {
                 for (let i = 0; i<5; i++){
-                        console.log(playRound(playerSelection, computerPlayer()));
-                } if (true) {
-                        playerScore++
-                } else if (false){
-                        computerScore++
-                } else {
-                        drawCounter++
+                        
+                      let result = playRound(playerSelection, computerPlayer()); //playround
+                      if (result === true) {
+                              playerScore++
+                      } else if (result === false){
+                              computerScore++
+                      } else drawCounter++
+               
                 }
+                if (playerScore>computerScore){
+                        return "well done! You have won the game!"
+                } else if (playerScore<computerScore){
+                        return "Unlucky!, you lost good looking!"
+                } else return "No one won, you are both equally awful!"
         }
          
                           
