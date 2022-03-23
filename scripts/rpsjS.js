d79debb6673ed4673295f621b747d204ea810192
Connor Davies <venexconsultant@outlook.com>
2022-03-23 21:00:38 +0000

    <update> updated for loop and gameRound() function.

--- scripts/rpsjS.js
+++ scripts/rpsjS.js
@@ -2,58 +2,77 @@
 // create a function that returns at random, rock paper or scissors. 
 // computerPlayer should be first function 
 
-let playerScore = 0;
-let computerScore = 0; 
+
 
         function computerPlayer() {
-        let rPS = ["rock", "paper", "scissors"];
+        let rPS = ["ROCK", "PAPER", "SCISSORS"];
         let compChoice = Math.floor(Math.random() * rPS.length);
         
-        return compChoice == 0 ? "rock" :
-                compChoice == 1 ? "Scissors" :
-                "Paper";   
+        return compChoice == 0 ? "ROCK" :
+                compChoice == 1 ? "SCISSORS" :
+                "PAPER";   
         }
         
 // computerSelection = computerPlay(); 
         computerSelection = computerPlayer(); 
-        playerSelection = "scissors"; 
+        playerSelection = prompt(); 
 
 // playRound decides the winner
         function playRound(playerSelection, computerSelection) {
                 if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-                        return "DRAW!, Please try again"; 
+                        console.log("DRAW!, Please try again");
+                        return null; 
                 } else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "SCISSORS"){
                         console.log("Nice! You have won this round!")
-                          ++playerScore;
+                        return true;
                 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
                         console.log("Excellent, good job, you have won!")
-                          ++playerScore;
+                        return true;
                 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
                         console.log("Great job! you rock!")
-                          ++playerScore;
+                        return true;
                 }  else if (playerSelection.toUpperCase()=== "ROCK" && computerSelection.toUpperCase()=== "PAPER"){
                         console.log("Unlucky you lost!")
-                          ++computerScore;
+                        return false;
                 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
                         console.log("Too bad, better luck next time")
-                         ++computerScore;
+                        return false;;
                 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") 
                         console.log("Better luck next time buddy!")
-                          ++computerScore;
+                        return false;
 
-        }
+}
+               
 
 
 
 // results logged in console
-        console.log(playerSelection);
-        console.log(computerSelection)
-        console.log(playerScore)
-        console.log(computerScore)
+       // console.log(playerSelection);
+        //console.log(computerSelection)
+        //console.log(playerScore)
+        //console.log(computerScore)
          
 
 // gameRound() Function Dec 
+let playerScore = 0;
+let computerScore = 0; 
+let drawCounter = 0; 
 
-        function gameRound(playerChoice) {
 
+        function gameRound() {
+                for (let i = 0; i<5; i++){
+                        console.log(playRound(playerSelection, computerPlayer()));
+                } if (true) {
+                        playerScore++
+                } else if (false){
+                        computerScore++
+                } else {
+                        drawCounter++
+                }
         }
+         
+                          
+        console.log(gameRound())
+        console.log(playerScore)
+        console.log(computerScore)
+        console.log(drawCounter)
